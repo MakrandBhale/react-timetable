c620e5f48462da8393561dd5b78fb72abb00dab3
makarand <makarandbhale123@gmail.com>
2020-08-21 19:41:27 +0530

    Updates

---
+++ precache-manifest.ab938a69cbefbae34afee32206f77faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1438583a512040b3576cafc1d5989d",
+    "url": "/react-timetable/index.html"
+  },
+  {
+    "revision": "abde30f273d8f51e5b71",
+    "url": "/react-timetable/static/css/main.2c46eced.chunk.css"
+  },
+  {
+    "revision": "31202226d083ebd8ea54",
+    "url": "/react-timetable/static/js/2.f8bd2205.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-timetable/static/js/2.f8bd2205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abde30f273d8f51e5b71",
+    "url": "/react-timetable/static/js/main.a6b86d47.chunk.js"
+  },
+  {
+    "revision": "05308d551c7fc55be1db",
+    "url": "/react-timetable/static/js/runtime-main.256cf90c.js"
+  }
+]);
